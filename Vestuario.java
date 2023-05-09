26f13ce184adf2fd14b2eb94b28b00255956b367
Bruno Coelho <brunocoelhonet@gmail.com>
2023-05-09 16:09:47 -0300

    cria classe vestuario

---
+++ Vestuario.java
@@ -0,0 +1,23 @@
+public class Vestuario extends Loja{
+
+    private boolean produtosImportados;
+
+    public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, boolean produtosImportados) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+    public boolean isProdutosImportados() {
+        return produtosImportados;
+    }
+
+    public void setProdutosImportados(boolean produtosImportados) {
+        this.produtosImportados = produtosImportados;
+    }
+
+    @Override
+    public String toString() {
+        return "Vestuario{" + super.toString() +
+                "produtosImportados=" + produtosImportados +
+                '}';
+    }
+}
