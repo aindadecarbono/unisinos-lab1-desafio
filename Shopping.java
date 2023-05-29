2b308947d9f973fc35b78cbc94e0f2bb3633fb60
Bruno Coelho <brunocoelhonet@gmail.com>
2023-05-29 15:21:28 -0300

    cria classe Shopping

---
+++ Shopping.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Shopping {
+    private String nome;
+    private Endereco endereco;
+    private Loja[] lojas;
+
+    public Shopping(String nome, Endereco endereco, int quantidadeLojas) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.lojas = new Loja[quantidadeLojas];
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Loja[] getLojas() {
+        return lojas;
+    }
+
+    public void setLojas(Loja[] lojas) {
+        this.lojas = lojas;
+    }
+
+    @Override
+    public String toString() {
+        return "Shopping{" +
+                "nome = " + nome + "\n" +
+                "endereco = " + endereco + "\n" +
+                "lojas = " + Arrays.toString(lojas) +
+                '}';
+    }
+}
